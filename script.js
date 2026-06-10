090bef0537faecf4373e0a898d698084e45f328f
Karla G. <k.grajalesluna@gmail.com>
2026-06-10 23:02:11 +0100

    chore: add js browser environment exercises

---
+++ script.js
@@ -0,0 +1,51 @@
+// Lesson 1
+// using the console on the browser to do some simple calculations and display messages in popups.
+
+// 1. use alert(...); to display 'Good Morning'
+
+alert('Good Morning');
+
+// 2. Display your name in a popup
+
+alert('Veronica');
+
+// 3. using math, calculate 10 + 5 in the console
+
+10 + 5;
+
+// 4. calculate 20 - 5 in the console
+
+20 - 5;
+
+// 5. calculate 20 + 2 in the console
+
+20 + 2; 
+
+// 6. Use the document.body.innerHTML to display 'Good Morning' on the webpage
+
+document.body.innerHTML = 'Good Morning';
+
+// 7. Use the document.body.innerHTML to display your name on the webpage
+
+document.body.innerHTML = 'Veronica';
+
+// 8. you order a T-shirt for $10, socks for $8, and dinner plates for $20. Use
+// Js to calculate the total cost of your order.
+
+let tshirt = 10;
+let socks = 8;
+let dinnerPlates = 20;
+let totalCost = tshirt + socks + dinnerPlates;
+console.log(totalCost);
+
+
+// 9. Your bank account has $100, you spend $20 on lunch, $50, on dinnerPlates,
+// amd earn $200 from your PackJob. Calculate how much money you have.$100
+
+let bankAccount = 100;
+
+// 10. Use document.body.innerHTML = ...; to make the web blank.
+
+document.body.innerHTML = '';
+
+
