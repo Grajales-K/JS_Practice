811b1c460638ef45a90e06d231be6ab1467b8d03
Karla G. <k.grajalesluna@gmail.com>
2026-08-21 09:20:47 +0200

    feat: implement render function for todo list and update display logic

--- lesson11-arrays&Loops/scripts/11-todo-list.js
+++ lesson11-arrays&Loops/scripts/11-todo-list.js
@@ -1,5 +1,23 @@
-const todoList = [];
+const todoList = ['run', ['swim']];
+
+renderToddoList();
 console.log(todoList);
+
+function renderToddoList(){
+
+    let todoListHTML = '';
+
+    for (let i = 0; i < todoList.length; i++){
+        const todo = todoList[i];
+        const html = `<p>${todo}<p>`;
+        todoListHTML += html;
+    }
+
+    console.log(todoListHTML);
+
+    document.querySelector('.js-todo-list').innerHTML = todoListHTML = todoListHTML;
+
+}
 
 function addTodo(todo){
     const inputElement = document.querySelector(".js-name-input");
@@ -9,7 +27,7 @@ function addTodo(todo){
     console.log(todoList);
 
     inputElement.value = "";
-
+    renderToddoList();
 
 }
 
