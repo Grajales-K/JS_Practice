f5f842bc7ed9aca5218fe58ead14b89d05a41c52
Karla G. <k.grajalesluna@gmail.com>
2026-09-02 20:38:00 +0200

    feat: add delete button functionality to todo list items

--- lesson11-arrays&Loops/scripts/11-todo-list.js
+++ lesson11-arrays&Loops/scripts/11-todo-list.js
@@ -9,7 +9,14 @@ function renderToddoList(){
 
     for (let i = 0; i < todoList.length; i++){
         const todo = todoList[i];
-        const html = `<p>${todo}<p>`;
+        const html = 
+            `<p>
+                ${todo}
+                <button onclick="
+                    todoList.splice(${i}, 1);
+                    renderToddoList();
+                ">Delete</button>
+            <p>`;
         todoListHTML += html;
     }
 
