93ca53b3fe9f55c92b6253dd5fc7a9746b9e241b
Karla G. <k.grajalesluna@gmail.com>
2026-09-02 21:47:09 +0200

    feat: add due date input and update todo list structure to include due dates

--- lesson11-arrays&Loops/scripts/11-todo-list.js
+++ lesson11-arrays&Loops/scripts/11-todo-list.js
@@ -1,4 +1,12 @@
-const todoList = ['run', ['swim']];
+const todoList = [{
+     name:'run',
+     dueDate: '2024-06-30',
+
+    }, {
+        
+     name: 'swim',
+     dueDate: '2024-07-01',
+    }];
 
 renderToddoList();
 console.log(todoList);
@@ -8,16 +16,19 @@ function renderToddoList(){
     let todoListHTML = '';
 
     for (let i = 0; i < todoList.length; i++){
-        const todo = todoList[i];
-        const html = 
-            `<p>
-                ${todo}
+      const todoObject = todoList[i];
+      // const name = todoObject.name;
+      // const dueDate = todoObject.dueDate;
+      const { name, dueDate } = todoObject; // Destructuring method.
+
+      const html = `<p>
+                ${name} ${dueDate}
                 <button onclick="
                     todoList.splice(${i}, 1);
                     renderToddoList();
                 ">Delete</button>
             <p>`;
-        todoListHTML += html;
+      todoListHTML += html;
     }
 
     console.log(todoListHTML);
@@ -27,15 +38,23 @@ function renderToddoList(){
 }
 
 function addTodo(todo){
-    const inputElement = document.querySelector(".js-name-input");
-    const name = inputElement.value;
+  const inputElement = document.querySelector('.js-name-input');
+  const name = inputElement.value;
 
-    todoList.push(name);
-    console.log(todoList);
+  const dateInputElelement = documento.querySelectort('.js-due-date-input');
+  const dueDate = dateInputElelement.value;
 
-    inputElement.value = "";
-    renderToddoList();
+  // todoList.push({ name: name, dueDate: dueDate });
+  //shorthand property method, if the key and value are the same, you can just write the key name.
+  todoList.push({
+    name,
+    dueDate
+  });
 
+  console.log(todoList);
+
+  inputElement.value = '';
+  renderToddoList();
 }
 
 
