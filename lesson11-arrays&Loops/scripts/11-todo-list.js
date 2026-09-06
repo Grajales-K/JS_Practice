0b0c234c5699b253d4bbd37d0ac668a5afd91888
Karla G. <k.grajalesluna@gmail.com>
2026-09-06 17:03:38 +0200

    feat: implement localStorage functionality for persistent todo list

--- lesson11-arrays&Loops/scripts/11-todo-list.js
+++ lesson11-arrays&Loops/scripts/11-todo-list.js
@@ -1,4 +1,6 @@
-const todoList = [
+// / 11x update the todList to use localStorage (if we modify the todoList and refresh the page, the todo list should stay the same. you can use JSON.stringify() and JSON.parse() to store and retrieve the todoList from localStorage).
+
+let todoList = JSON.parse(localStorage.getItem('todoList')) || [
   {
     name: 'run',
     dueDate: '2024-06-30'
@@ -8,6 +10,7 @@ const todoList = [
     dueDate: '2024-07-01'
   }
 ];
+
 
 renderToddoList();
 console.log(todoList);
@@ -26,6 +29,7 @@ function renderToddoList() {
                 <div>${dueDate}</div>
                 <button onclick="
                     todoList.splice(${i}, 1);
+                    saveToStorage(); 
                     renderToddoList();
                     " class="delete-todo-button">Delete</button>`;
     todoListHTML += html;
@@ -33,16 +37,16 @@ function renderToddoList() {
 
   console.log(todoListHTML);
 
-  document.querySelector('.js-todo-list').innerHTML = todoListHTML =
-    todoListHTML;
+  document.querySelector('.js-todo-list').innerHTML = todoListHTML;
 }
 
-function addTodo(todo) {
+function addTodo() {
   const inputElement = document.querySelector('.js-name-input');
   const name = inputElement.value;
 
   const dateInputElelement = document.querySelector('.js-due-date-input');
-  const dueDate = dateInputElelement.value;
+  const dueDate = dateInputElelement.value || 'no date';
+
 
   // todoList.push({ name: name, dueDate: dueDate });
   //shorthand property method, if the key and value are the same, you can just write the key name.
@@ -54,5 +58,14 @@ function addTodo(todo) {
   console.log(todoList);
 
   inputElement.value = '';
+  dateInputElelement.value = '';
+
+  //To avoid the element being lost when the page is refreshed, we add saveToStorage() function to store the todoList in the browser's memory.
+  saveToStorage();
   renderToddoList();
+
+}
+
+function saveToStorage() {
+  localStorage.setItem('todoList', JSON.stringify(todoList));
 }
