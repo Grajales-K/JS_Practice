4b8a5fcac66a4c58320553444b8506501385ed99
Karla G. <k.grajalesluna@gmail.com>
2026-09-10 08:54:57 +0200

    feat: implement toggle functionality for auto play in Rock-Paper-Scissors game

--- lesson12/scripts/12-rock-paper-scissors.js
+++ lesson12/scripts/12-rock-paper-scissors.js
@@ -17,11 +17,20 @@ updateScoreElement();
         }
         */
 
+let isAutoPlaying = false;
+let intervalId;
+
 function autoPlay() {
-  setInterval( function() {
-    const playerMove = pickComputerMove();
-    playGame(playerMove);
-  }, 1000);
+  if (!isAutoPlaying) {
+    intervalId = setInterval(function () {
+      const playerMove = pickComputerMove();
+      playGame(playerMove);
+    }, 1000);
+    isAutoPlaying = true;
+  } else {
+    clearInterval(intervalId);
+    isAutoPlaying = false;
+  }
 }
 
 function playGame(playerMove) {
