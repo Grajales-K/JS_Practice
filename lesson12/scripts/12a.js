74cc8f49a0c9314df2d63345d72cacb21b701203
Karla G. <k.grajalesluna@gmail.com>
2026-09-12 19:06:56 +0200

    feat: new setTimeout to display and remove message

--- lesson12/scripts/12a.js
+++ lesson12/scripts/12a.js
@@ -1,45 +1,42 @@
 console.log('======== 12a. function running twice ========');
 // 12a. Create a variable called 'add' and save a function inside. This function will console.log(2+3); run the function twice.
 
-const add = function() {
-    console.log(2 + 3);
+const add = function () {
+  console.log(2 + 3);
 };
 
 add(); // Call the function once
 add(); // Call the function again
 
-
 console.log('======== 12b. function running twice ========');
 /* 12b. continuing from the exercise above, create a function runtwice(fn) that takes a function (as parameter) and runs it twice.
-* runTwice(funtion(){console.log(12b)}); => console.log(12b) twice.
-* runTwice(add); => console.log(5) twice. */
+ * runTwice(funtion(){console.log(12b)}); => console.log(12b) twice.
+ * runTwice(add); => console.log(5) twice. */
 
 //we pass a anonymous function to runTwice that logs '12b' to the console
-runTwice(function() {
-    console.log('12b');
+runTwice(function () {
+  console.log('12b');
 }); //12b, 12b
 
-function runTwice(fn){
-    fn()
-    fn()
-};
+function runTwice(fn) {
+  fn();
+  fn();
+}
 
-runTwice(add) //5, 5
-
+runTwice(add); //5, 5
 
 console.log('======== 12c. button with event ========');
 //12c.  create a button. When clicking the button, after 1 second, the text inside the button changes to 'finished', use setTimeout() and DOM.
-
 
 const container = document.querySelector('.event-btn');
 const button = document.createElement('button');
 button.textContent = 'Start';
 container.appendChild(button);
 
-button.addEventListener('click', function(){
-    setTimeout(function(){
-        button.textContent = 'Finished';
-    }, 1000);
+button.addEventListener('click', function () {
+  setTimeout(function () {
+    button.textContent = 'Finished';
+  }, 1000);
 });
 
 console.log('======== 12d. button with event ========');
@@ -50,9 +47,33 @@ const button2 = document.createElement('button');
 button2.textContent = 'start';
 container2.appendChild(button2);
 
-button2.addEventListener('click', function(){
-    button2.textContent = 'loading...';
-    setTimeout(function(){
-        button2.textContent = 'finished';
-    }, 1000);
+button2.addEventListener('click', function () {
+  button2.textContent = 'loading...';
+  setTimeout(function () {
+    button2.textContent = 'finished';
+  }, 1000);
 });
+
+console.log('======== 12e. button event message ========');
+/*12e. Create a 'Add to Cart' button. When clicking the button, display the message 'added' below the button. Then, after 2 seconds, remove the message. */
+
+const container3 = document.querySelector('.add-btn');
+const button3 = document.createElement('button');
+button3.textContent = 'Add to Cart';
+container3.appendChild(button3);
+
+button3.addEventListener('click', function () {
+  const message = document.createElement('p');
+  message.textContent = 'Added';
+  container3.appendChild(message);
+
+  setTimeout(function () {
+    // container3.removeChild(message);
+    message.textContent = '';
+  }, 2000);
+});
+
+
+
+
+
